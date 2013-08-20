b6fea854a6e6de9ebdbf90df1693b52a05f8197b
AlexsJones <jonesax@hush.com>
2013-08-20 13:59:12 +0100

    updated

--- src/utils/terminal.c
+++ src/utils/terminal.c
@@ -29,14 +29,16 @@ void terminal_print(mysql_result_bucket *jobbucket)
 {
 	system("clear");
 	printf("\n");
-	printf("=====================================================================================================================\n");
+	printf("===========================================================================================================================\n");
 	printf("%-20s\t%-20s\t\t%-20s\t%-20s\t%-5s\n", "JOB ID","STATUS","NEXT RUN TIME","MACHINE ID", "TRIGGER DIFF");
 	int y;
+	long int last_time = -1;
 	for(y = 0; y < jobbucket->row_count; ++y)
 	{
-	printf("%-20s\t%-20s\t\t%-20s\t%-20s\t%-5d", jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"id")], jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"status")], jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"TIMESTAMP")],jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"machine_id")],check_time(atoi(jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"trigger_time")])));
-	printf("\n");
+		long int candidate_trigger = atoi(jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"trigger_time")]);
+		printf("%-20s\t%-20s\t\t%-20s\t%-20s\t%-5ld", jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"id")], jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"status")], jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"TIMESTAMP")],jobbucket->rows[y][get_mysql_result_bucket_field_position(&jobbucket,"machine_id")],check_time(candidate_trigger));
+		printf("\n");
 	}
-	printf("====================================================================================================================\n");
+	printf("===========================================================================================================================\n");
 	printf("\n");
 }
